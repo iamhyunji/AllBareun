96a7279881d8fcd79b93e8445defdbaab34b3863
cyon13 <bonyul13@gmail.com>
2021-01-18 18:24:36 +0900

    first commit

---
+++ src/main/java/com/allbareun/web/AllBareunApplication.java
@@ -0,0 +1,13 @@
+package com.allbareun.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllBareunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllBareunApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/allbareun/web/AllBareunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.allbareun.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllBareunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
