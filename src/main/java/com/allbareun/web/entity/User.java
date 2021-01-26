a0cd497443fbc6a1cfe12450967f9b934c6bb669
Hyun <iamhyunji_kim@icloud.com>
2021-01-26 22:04:49 +0900

    [현지] Entity 생성
    	- User
    	- Group

---
+++ src/main/java/com/allbareun/web/entity/User.java
@@ -0,0 +1,115 @@
+package com.allbareun.web.entity;
+
+import java.util.Date;
+
+public class User {
+	private int id;
+	private String name;
+	private String email;
+	private String password;
+	private String gender;
+	private Date joinDate;
+	private String profile;
+	private int age;
+	private String phone;
+	
+	// --------------------------------- Constructor ---------------------------------
+	public User() {
+		this(0, null, null, null, null, null, null, 0, null);
+	}
+
+	public User(int id, String name, String email, String password, String gender, Date joinDate, String profile,
+			int age, String phone) {
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.password = password;
+		this.gender = gender;
+		this.joinDate = joinDate;
+		this.profile = profile;
+		this.age = age;
+		this.phone = phone;
+	}
+
+	// --------------------------------- Getters/Setters ---------------------------------
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public Date getJoinDate() {
+		return joinDate;
+	}
+
+	public void setJoinDate(Date joinDate) {
+		this.joinDate = joinDate;
+	}
+
+	public String getProfile() {
+		return profile;
+	}
+
+	public void setProfile(String profile) {
+		this.profile = profile;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	// --------------------------------- Function ---------------------------------
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", name=" + name + ", email=" + email + ", password=" + password + ", gender="
+				+ gender + ", joinDate=" + joinDate + ", profile=" + profile + ", age=" + age + ", phone=" + phone
+				+ "]";
+	}
+	
+}
