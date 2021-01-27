e482901281a89a8ce20af51f5ca03a930e697077
Hyun <iamhyunji_kim@icloud.com>
2021-01-27 17:08:57 +0900

    [현지] aside tiles

--- src/main/java/com/allbareun/web/controller/MyPageController.java
+++ src/main/java/com/allbareun/web/controller/MyPageController.java
@@ -1,5 +1,27 @@
 package com.allbareun.web.controller;
 
-public class MyPageController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+//localhost8080:/mypage/done/list
+@Controller
+@RequestMapping("/mypage/")
+public class MyPageController {
+	
+	@GetMapping("done/list")
+	public String doneList() {
+		return "user.mypage.done.list";
+	}
+	
+	@GetMapping("done/detail")
+	public String doneDetail() {
+		return "user.mypage.done.detail";
+	}
+	
+	@GetMapping("report/result")
+	public String reportResult() {
+		return "user.mypage.report.result";
+	}
+	
 }
