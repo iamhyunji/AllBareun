3c4c144834345fd6f4203f8031f7d086cec888f1
HYEYOUNGLIM <20528young@gmail.com>
2021-02-09 16:54:15 +0900

    ..

--- src/main/resources/static/js/user/mypage/done/imageAnimation.js
+++ src/main/resources/static/js/user/mypage/done/imageAnimation.js
@@ -1,11 +1,11 @@
-
-/*import VideoPlayer from "./VideoPlayer.js";
+import VideoPlayer from "./VideoPlayer.js";
 
 window.addEventListener("load", function () {
   const videoSection = document.querySelector(".video-section");
   const videoFrame = document.querySelectorAll(".video-frame");
   const imgTag = document.querySelectorAll(".video-img");
   const stopIcon = document.querySelectorAll(".video-stop");
+
   let imgArrs = [];
   imgArrs[0] = ["/images/all.png", "/images/naver.png", "/images/all.png", "/images/naver.png", "/images/all.png"];
   imgArrs[1] = ["/images/naver.png", "/images/kakao.png", "/images/naver.png", "/images/kakao.png", "/images/kakao.png", "/images/naver.png"];
@@ -15,4 +15,4 @@ window.addEventListener("load", function () {
     videoPlayers[i] = new VideoPlayer(imgArrs[i], i, videoFrame, imgTag, stopIcon);
   }
 
-});*/
+});
